9f29f859715027c5ba11a5a6cd989987e4698001
skdbsqls <skdbsqls98@naver.com>
2023-11-13 23:29:03 +0900

    feat(App): 방문 날짜 선택 및 메뉴 주문 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import InputView from "./views/InputView.js";
+import OutputView from "./views/OutputView.js";
+
 class App {
-  async run() {}
+  #date;
+  #menu;
+
+  async run() {
+    await this.#executeIntro();
+  }
+
+  // 인트로 (방문 날짜 선택 및 메뉴 주문)
+  async #executeIntro() {
+    OutputView.printIntro();
+    this.#date = await InputView.readDate();
+    this.#menu = await InputView.readMenu();
+    OutputView.printPreview(this.#date);
+  }
 }
 
 export default App;
