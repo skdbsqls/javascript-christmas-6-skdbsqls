60c95b851e72377aea9c7629a43ed856686e34a8
skdbsqls <skdbsqls98@naver.com>
2023-11-14 04:23:03 +0900

    feat(message): 에러 메시지 추가

--- src/constants/message.js
+++ src/constants/message.js
@@ -19,6 +19,9 @@ const OUTPUT_MESSAGE = Object.freeze({
 const ERROR_MESSAGE = Object.freeze({
   invalid_date: "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.",
   invalid_menu: "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.",
+  only_beverage: "[ERROR] 음료만 주문할 수 없습니다. 다시 입력해 주세요.",
+  exceed_max:
+    "[ERROR] 메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다. 다시 입력해 주세요.",
 });
 
 export { INPUT_MESSAGE, OUTPUT_MESSAGE, ERROR_MESSAGE };
