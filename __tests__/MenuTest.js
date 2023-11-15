0a81f2149a49c8b3bd198ad01493cf484dacb0c5
skdbsqls <skdbsqls98@naver.com>
2023-11-15 20:23:34 +0900

    test: 메뉴 클래스 테스트

---
+++ __tests__/MenuTest.js
@@ -0,0 +1,25 @@
+import Menu from "../src/damains/Menu.js";
+
+describe("메뉴 클래스 테스트", () => {
+  test("정보 가져오기", () => {
+    const input = { name: "해산물파스타", count: 1 };
+    const output = {
+      course: "main",
+      name: "해산물파스타",
+      price: 35000,
+      count: 1,
+    };
+
+    const result = new Menu(input).getMenuInfo();
+
+    expect(result).toStrictEqual(output);
+  });
+  test("총금액 가져오기", () => {
+    const input = { name: "해산물파스타", count: 1 };
+    const output = 35000;
+
+    const result = new Menu(input).getMenuPrice();
+
+    expect(result).toBe(output);
+  });
+});
